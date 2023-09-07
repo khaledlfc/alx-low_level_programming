003d412407e85b7a40e62ebe85d168757eea9628
khaledlfc <khaled.2016@gmail.com>
2023-09-07 14:08:11 -0700

    updates

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+/**
+ * main - entry point
+ * 
+ * Return: always 0 (success)
+ */
+int main()
+{
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return 0;
+}
